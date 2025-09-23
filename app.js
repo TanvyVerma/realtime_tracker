38a8bd84879f05c4df27b39f5ed916cc6206405c
Tanvy Verma <tanvy@tanvy.com>
2025-09-23 22:40:41 +0530

    setup socket.io

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.get("/", (req, res) => {
-    console.log("User accessed the home page");
-});
+const http = require("http");
+const socketio = require("socket.io")
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.listen(3000, () => console.log("Server running..."));
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
+
+
+
+app.get("/", function(req, res) {
+    res.send("hey");
+})
+
+server.listen(3000, () => console.log("Server running..."));
+
